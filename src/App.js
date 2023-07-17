cc6a5b02ae079639fda7351ae78db5ac2698ef18
kannabiraninfo <kannabiran@infographicanalytics.com>
2023-07-17 16:50:59 +0530

    chnages done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './App.css';
 import Login from './login/login';
-import Footer from './footer/footer';
 
 
 function App() {
   return (
     <div className="App">
       <Login />
-      <Footer />
     </div>
   );
 }
--- src/login/login.js
+++ src/login/login.js
@@ -14,12 +14,12 @@ function login() {
   return (
     <Container fluid>
       <Row>
-      <Col lg="6" style={{background:"#eee",height:"100vh"}}>
+      <Col lg="6" style={{background:"#eee",margin:"10px 0"}}>
       
       <Form className='box center desktop-vmiddle'>
           <Width width="100%">
            <Image src="logo.png" rounded className='login-logo' />
-           <TextAlign alignment="center"><Text fontWeight="bold"><FontSize fontSize="22px">NAC IntelliDash</FontSize></Text></TextAlign>
+           <TextAlign alignment="center"><Text fontWeight="bold"><FontSize fontSize="22px"> NAC IntelliDash</FontSize></Text></TextAlign>
             <Form.Group className="mb-3" controlId="formGroupEmail">
               <TextAlign alignment="left">Email address</TextAlign>
               <Form.Control type="email" placeholder="Enter email" style={{padding:"10px"}}/>
@@ -29,6 +29,11 @@ function login() {
               <Form.Control type="password" placeholder="Password" style={{padding:"10px"}}/>
             </Form.Group>
             <Form.Group>
+              <Text>
+                <Color color="red">
+                  fhsfks
+                </Color>
+              </Text>
           <Col sm={{ span: 12,}}>
             <Button type="submit" className='width100'><Padding size="5px">Sign in <FontAwesomeIcon icon={faSignIn} /> </Padding></Button>
           </Col>
@@ -36,38 +41,39 @@ function login() {
         </Width>
       </Form>
     </Col>
-    <Col lg="6" style={{background: "#383990"}}>
+    <Col lg="6" style={{background: "#383990",height:"100vh"}} className='mobpad'>
       <Padding size="25px">
       <TextAlign  alignment="left">
         <Color color="white">
-          <Width width="25%">
+          <Width width="300px">
             <TextAlign alignment="center">
-              <Card.Text><FontSize fontSize="25px"><Color color="#3ee3ff"> <FontAwesomeIcon icon={faChartBar}  /></Color></FontSize> Operational Reports</Card.Text>
+              <Card.Text><Padding size="25px"><FontSize fontSize="25px"><Color color="#3ee3ff"> <FontAwesomeIcon icon={faChartBar}  /></Color></FontSize> Operational Reports</Padding></Card.Text>
             </TextAlign>
           </Width>
-          <Width width="25%">
+          <Width width="300px">
             <TextAlign alignment="center">
-              <Card.Text><FontSize fontSize="25px"><Color color="#3ee3ff"><FontAwesomeIcon icon={faChartColumn}  /></Color></FontSize>Financial Reports</Card.Text>
+              <Card.Text><Padding size="25px"><FontSize fontSize="25px"><Color color="#3ee3ff"><FontAwesomeIcon icon={faChartColumn}  /></Color></FontSize>Financial Reports</Padding></Card.Text>
             </TextAlign>
           </Width>
-          <Width width="25%">
+          <Width width="300px">
             <TextAlign alignment="center">
-              <Card.Text><FontSize fontSize="25px"><Color color="#3ee3ff"><FontAwesomeIcon icon={faChartGantt}  /></Color></FontSize>Performance Reports</Card.Text>
+              <Card.Text><Padding size="25px"><FontSize fontSize="25px"><Color color="#3ee3ff"><FontAwesomeIcon icon={faChartGantt}  /></Color></FontSize>Performance Reports</Padding></Card.Text>
             </TextAlign>
           </Width>
-          <Width width="25%">
+          <Width width="300px">
           <TextAlign alignment="center">
-            <Card.Text><FontSize fontSize="25px"><Color color="#3ee3ff"><FontAwesomeIcon icon={faChartPie}  /></Color></FontSize>Analytical Reports</Card.Text>
+            <Card.Text><Padding size="25px"><FontSize fontSize="25px"><Color color="#3ee3ff"><FontAwesomeIcon icon={faChartPie}  /></Color></FontSize>Analytical Reports</Padding></Card.Text>
             </TextAlign>
           </Width>
-          <Width width="25%">
+          <Width width="300px">
           <TextAlign alignment="center">
-            <Card.Text><FontSize fontSize="25px"><Color color="#3ee3ff"><FontAwesomeIcon icon={faChartSimple}  /></Color></FontSize>Executive Dashboard</Card.Text>
+            <Card.Text><Padding size="25px"><FontSize fontSize="25px"><Color color="#3ee3ff"><FontAwesomeIcon icon={faChartSimple}  /></Color></FontSize>Executive Dashboard</Padding></Card.Text>
             </TextAlign>
           </Width>
         </Color> 
       </TextAlign>
       </Padding>
+      <Image src='man-img.png' rounded className='man-img'/>
     </Col>
     </Row>
     </Container>
