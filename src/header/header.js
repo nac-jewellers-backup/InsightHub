dc9dd5b5057fc11791ebe2fcb525cbfee04e0f0f
kannabiraninfo <kannabiran@infographicanalytics.com>
2023-07-19 18:23:45 +0530

    changes done by kannna

--- src/header/header.js
+++ src/header/header.js
@@ -1,7 +1,8 @@
+import { useState } from 'react';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './header.css';
-import { Col, Container, Row, Image, Nav, Navbar, Button } from 'react-bootstrap';
+import { Col, Container, Row, Image, Nav, Navbar, Button, Modal  } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHouse, faSignOut, faChartArea } from '@fortawesome/free-solid-svg-icons';
 import { resetUserSession } from '../misc/authService';
@@ -9,9 +10,11 @@ import { resetUserSession } from '../misc/authService';
 export default function Header() {
 	const navigate = useNavigate();
 	const logout = () => {
+		setShow(false)
 		resetUserSession();
 		navigate("/");
 	};
+	const [show, setShow] = useState(false);
 	return (
 		<Container fluid>
 			<Row>
@@ -20,7 +23,7 @@ export default function Header() {
 				</Col>
 				<Col lg={9}></Col>
 				<Col lg={1} className='center-box'>
-				<Button className='center-button' onClick={logout} ><FontAwesomeIcon icon={faSignOut}/> Logout</Button>
+				<Button className='center-button' onClick={()=>setShow(true)} ><FontAwesomeIcon icon={faSignOut}/> Logout</Button>
 				</Col>
 			</Row>
 			<Row>
@@ -38,6 +41,24 @@ export default function Header() {
 						</Navbar.Collapse>
 					</Container>
 				</Navbar>
+			</Row>
+			<Row>
+			
+      <Modal show={show} onHide={()=>setShow(false)} animation={false} >
+        <Modal.Header closeButton className='text-center'>
+          <Modal.Title className='text-center text-bold'>Logout ?</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className='text-center exit'>Do you want logout </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" className='cutomof' onClick={()=>setShow(false)}>
+            No
+          </Button>
+          <Button variant="danger" className='cutomof' onClick={logout}>
+            Yes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
 			</Row>
 		</Container>
 	);
