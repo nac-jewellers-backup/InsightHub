fe256a853d595b398bf8dee2f8591aaf81ce5996
raman-rv <ramanrv@infographicanaltics.com>
2023-07-20 18:41:34 +0530

    iframe height changed

--- src/salesreport/salesreport.js
+++ src/salesreport/salesreport.js
@@ -26,7 +26,7 @@ export default function SalesReport() {
             <div>
                 <iframe
                     width={windowWidth - 15}
-                    height={windowHeight - 210}
+                    height={windowHeight - 225}
                     title='NAC Reports'
                     src='https://us-east-2.quicksight.aws.amazon.com/sn/embed/share/accounts/185714328388/dashboards/931753bf-95c1-4f72-a77d-44145cd5e4bc?directory_alias=infographicanalytics'
                 />
